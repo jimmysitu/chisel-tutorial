c0c2491c31441e71154faf7d4ad08eb1f7f11ae4
Jimmy Situ <web@jimmystone.cn>
2017-04-19 20:58:47 +0800

    Mux4 done, Max2 done

--- src/test/scala/problems/Max2Tests.scala
+++ src/test/scala/problems/Max2Tests.scala
@@ -11,11 +11,13 @@ class Max2Tests(c: Max2) extends PeekPokeTester(c) {
   for (i <- 0 until 10) {
 
     // Implement below ----------
+    val in0 = rnd.nextInt(256)
+    val in1 = rnd.nextInt(256)
 
-    poke(c.io.in0, 0)
-    poke(c.io.in1, 0)
+    poke(c.io.in0, in0)
+    poke(c.io.in1, in1)
     step(1)
-    expect(c.io.out, 1)
+    expect(c.io.out, if (in0 > in1) in0 else in1)
 
     // Implement above ----------
   }
